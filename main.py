37f3f35ccd994e3dd9873f4363946f029171e531
aureliobarrios <aubarrios142@gmail.com>
2025-10-02 23:43:32 -0700

    tracker include

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from utils import read_video, save_video
+from trackers import Tracker
 
 def main():
     #read video
     video_frames = read_video("videos/match.mp4")
+
+    #initialize tracker
+    tracker = Tracker("models/best.pt")
+
+    tracks = tracker.get_object_tracks(video_frames)
 
     #save video
     save_video(video_frames, 'output_videos/output_video.avi')
