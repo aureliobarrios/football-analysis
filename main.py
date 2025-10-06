9bfcd07bf8ea13324d8638a7a057d7fbd7b8fa87
aureliobarrios <aubarrios142@gmail.com>
2025-10-06 15:58:27 -0700

    team clustering

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from utils import read_video, save_video
 from trackers import Tracker
+from team_assigner import TeamAssigner
 import cv2
 
 def main():
@@ -23,6 +24,16 @@ def main():
     #     cv2.imwrite(f"output_videos/cropped_img.jpg", cropped_image)
 
     #     break
+
+    #Assign player teams
+    team_assigner = TeamAssigner()
+    team_assigner.assign_team_color(video_frames[0], tracks["players"][0])
+
+    for frame_num, player_track in enumerate(tracks["players"]):
+        for player_id, track in player_track.items():
+            team = team_assigner.get_player_team(video_frames[frame_num], track["bbox"], player_id)
+            tracks["players"][frame_num][player_id]["team"] = team
+            tracks["players"][frame_num][player_id]["team_color"] = team_assigner.team_colors[team]
 
     #draw output
     #draw object tracks
